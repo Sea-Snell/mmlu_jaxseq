5870ee413865bf3a7c185e84156e1acc6295dbcd
Charlie Snell <sea_snell@icloud.com>
2023-04-12 18:35:13 +0000

    mmlu evals

--- test.sh
+++ test.sh
@@ -1,5 +1,54 @@
 #!/bin/bash
 
+# 4/12/2023
+
+# nlp4 left pane
+# echo "koala"
+# python evaluate_eval_harness_jaxseq.py \
+#     --host http://127.0.0.1:8005/ \
+#     --k-shot 0 \
+#     --input-process koala \
+#     --bsize 4 
+# echo "koala"
+
+# nlp4 right pane
+# echo "llama"
+# python evaluate_llama_jaxseq.py \
+#     --name llama_13B_original_prompt_zero_shot \
+#     --host http://127.0.0.1:8004/ \
+#     --k-shot 0 \
+#     --data-dir /home/csnell/mmlu_easylm/data/ \
+#     --save-dir /home/csnell/mmlu_easylm/outputs/
+# echo "llama"
+
+# nlp3 top left pane
+# echo "koala 10k"
+# python evaluate_eval_harness_jaxseq.py \
+#     --host http://127.0.0.1:8006/ \
+#     --k-shot 0 \
+#     --input-process koala \
+#     --bsize 4 
+# echo "koala 10k"
+
+# nlp3 top right pane
+# echo "koala 50k"
+# python evaluate_eval_harness_jaxseq.py \
+#     --host http://127.0.0.1:8007/ \
+#     --k-shot 0 \
+#     --input-process koala \
+#     --bsize 4 
+# echo "koala 50k"
+
+# nlp3 botom pane
+# echo "koala 100k"
+# python evaluate_eval_harness_jaxseq.py \
+#     --host http://127.0.0.1:8008/ \
+#     --k-shot 0 \
+#     --input-process koala \
+#     --bsize 4 
+# echo "koala 100k"
+
+
 # 4/11/2023
 
 # left pane
